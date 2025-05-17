7eb720823e432b2bc13f0cb8c5ac8ac8d73234f5
Ale <alejandro79861@gmail.com>
2025-05-17 11:25:10 -0500

    fix: correct default search parameter from "query" to "search" in SearchInput component

--- src/components/Nav/SearchInput.tsx
+++ src/components/Nav/SearchInput.tsx
@@ -42,7 +42,7 @@ export function SearchInput() {
         onChange={(e) => {
           handleSearch(e.target.value);
         }}
-        defaultValue={searchParams.get("query")?.toString()}
+        defaultValue={searchParams.get("search")?.toString()}
       />
     </div>
   );
